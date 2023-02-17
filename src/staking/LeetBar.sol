7f921f5c6202311365c7b41e03faa23b7d9245ee
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2023-02-17 16:32:01 +0100

    feat: implement xLEET bar and CSR for it

---
+++ src/staking/LeetBar.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import "@leetswap/interfaces/ITurnstile.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+interface ICSRContract {
+    function turnstile() external view returns (ITurnstile);
+}
+
+contract LeetBar is ERC20("xLeet", "xLEET") {
+    using SafeMath for uint256;
+    IERC20 public leet;
+
+    constructor(IERC20 _leet) {
+        leet = _leet;
+        ITurnstile turnstile = ICSRContract(address(leet)).turnstile();
+        uint256 csrTokenID = turnstile.getTokenId(address(leet));
+        turnstile.assign(csrTokenID);
+    }
+
+    function enter(uint256 _tokenAmount) public {
+        uint256 totalLeet = leet.balanceOf(address(this));
+        uint256 totalShares = totalSupply();
+
+        if (totalShares == 0 || totalLeet == 0) {
+            _mint(msg.sender, _tokenAmount);
+        } else {
+            uint256 sharesToMint = _tokenAmount.mul(totalShares).div(totalLeet);
+            _mint(msg.sender, sharesToMint);
+        }
+
+        leet.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    function leave(uint256 _sharesAmount) public {
+        uint256 totalShares = totalSupply();
+        uint256 userTokens = _sharesAmount
+            .mul(leet.balanceOf(address(this)))
+            .div(totalShares);
+
+        _burn(msg.sender, _sharesAmount);
+        leet.transfer(msg.sender, userTokens);
+    }
+}
